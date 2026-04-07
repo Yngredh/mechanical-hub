3e3d9d68ff3a541dea286f7ab4b127949ff802c6
Yngredh <yngredh.cruz@gmail.com>
2026-04-07 11:48:01 -0300

    Initial commit - Project created with Spring Initilizr

---
+++ src/main/java/com/fiap/mechanical_hub/MechanicalHubApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.mechanical_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicalHubApplication.class, args);
+	}
+
+}
