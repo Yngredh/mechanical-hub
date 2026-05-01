dd114f579de38a9991ab025db933ea833abf353f
amanda <amandacdesousa@outlook.com>
2026-05-01 15:44:36 -0300

    adiccao builder servicematerial

--- src/main/java/com/fiap/mechanical_hub/domain/entities/ServiceMaterial.java
+++ src/main/java/com/fiap/mechanical_hub/domain/entities/ServiceMaterial.java
@@ -1,6 +1,7 @@
 package com.fiap.mechanical_hub.domain.entities;
 
 import com.fiap.mechanical_hub.domain.exceptions.BusinessRuleException;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
+@Builder
 public class ServiceMaterial {
 
     private UUID id;
